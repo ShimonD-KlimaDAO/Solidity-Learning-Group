808e0064118283adb4e69ed7501b75d2776846b7
ShimonD-KlimaDAO <99001851+ShimonD-KlimaDAO@users.noreply.github.com>
2022-03-21 12:13:14 -0700

    Update to call fallback function
    Example discussed in class

--- MaliciousCode/contracts/Malicious.sol
+++ MaliciousCode/contracts/Malicious.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.10;
 contract Malicious {
     event Log(string message);
 
-    //fallback () external {
-    //    emit Log("Malicious fallback was called");
-    //}
+    fallback () external {
+        emit Log("Malicious fallback was called");
+    }
 
     // Actually we can execute the same exploit even if this function does
     // not exist by using the fallback
--- MaliciousCode/scripts/deploy.py
+++ MaliciousCode/scripts/deploy.py
@@ -7,9 +7,9 @@ def main():
     mal = Malicious.deploy({"from": account})
     foo = Foo.deploy({"from": account})
     top_level_good = TopLevel.deploy(foo, {"from": account})
-    #top_level_bad = TopLevel.deploy(mal, {"from": account})
+    top_level_bad = TopLevel.deploy(mal, {"from": account})
     tx_good = top_level_good.callFoo()
-    #tx_bad = top_level_bad.callFoo()
+    tx_bad = top_level_bad.callFoo()
 
     print(tx_good.info())
-    #print(tx_bad.info())
+    print(tx_bad.info())
